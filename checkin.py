7dd842124b976c9e48b68555269d8900f918e73a
zhangchaowei <withqianqian@163.com>
2021-07-01 16:28:03 +0800

    删除log

--- checkin.py
+++ checkin.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import os
+import json
 
 requests.packages.urllib3.disable_warnings()
 
@@ -28,9 +29,13 @@ class SspanelQd(object):
                     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 }
 
-                post_data = 'email='+self.email[i]+'&passwd='+self.password[i]
+                post_data = 'email=' + self.email[i] + '&passwd=' + self.password[i]
                 post_data = post_data.encode()
                 response = session.post(login_url, post_data, headers=headers, verify=False)
+                login_result = json.loads(response.text)
+                if login_result.get('ret') == 1:
+                    print(self.base_url[i] + ' ' + '登陆失败')
+                    continue
 
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
@@ -40,7 +45,7 @@ class SspanelQd(object):
                 response = session.post(self.base_url[i] + '/user/checkin', headers=headers, verify=False)
                 msg = (response.json()).get('msg')
 
-                print(self.base_url[i]+' \n'+msg)
+                print(self.base_url[i] + ' \n' + msg)
 
         except Exception as e:
             msgall = '签到失败'
@@ -48,7 +53,7 @@ class SspanelQd(object):
         return msgall
 
     def main(self):
-        msg = self.checkin()
+        self.checkin()
 
 
 if __name__ == '__main__':
