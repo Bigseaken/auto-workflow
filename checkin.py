a0c91f832c0ddfcf83a5963a86a1c816b103146a
zhangchaowei <withqianqian@163.com>
2021-07-01 15:41:50 +0800

    test

--- checkin.py
+++ checkin.py
@@ -1,51 +1,50 @@
 import requests
 import re
 import os
-import json
 
+requests.packages.urllib3.disable_warnings()
 
 
 class SspanelQd(object):
     def __init__(self):
         # 机场地址
-        web_list = os.environ['web']
-        self.base_url = web_list.split(',')
+
+        self.base_url = os.environ['web'].split(',')[0]
         # 登录信息
-        user_list = os.environ['user']
-        self.email = user_list.split(',')
-        pwd_list = os.environ['pwd']
-        self.password = pwd_list.split(',')
+
+        self.email = os.environ['user'].split(',')[0]
+
+        self.password = os.environ['pwd'].split(',')[0]
 
     def checkin(self):
         msgall = '签到成功'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        }
         try:
-            # for i in range(len(self.base_url)):
-            i = 0
-            session = requests.session()
-            login_url = self.base_url[i] + '/auth/login'
-            post_data = 'email='+self.email[i]+'&passwd='+self.password[i]
-            post_data_z = post_data.encode()
-            response = session.post(login_url, post_data_z, headers=headers, verify=False,)
-            ret = json.loads(response.text).get('ret')
-            if ret == 1:
-                print(self.base_url[i] + ' login result=====>登陆失败 '+post_data)
-                return
-            else:
-                print(self.base_url[i] + ' login result=====>登陆成功')
 
-            referer_headers = {
+            session = requests.session()
+
+            login_url = self.base_url + '/auth/login'
+            headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Referer': self.base_url[i] + '/user'
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             }
 
-            response = session.post(self.base_url[i] + '/user/checkin', headers=referer_headers, verify=False )
+            post_data = 'email='+self.email+'&passwd='+self.password
+            post_data = post_data.encode()
+            response = session.post(login_url, post_data, headers=headers, verify=False)
+
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                'Referer': self.base_url + '/user'
+            }
+
+            response = session.post(self.base_url + '/user/checkin', headers=headers, verify=False)
             msg = (response.json()).get('msg')
+
+            msgall = msgall + self.base_url + '\n\n' + msg + '\n\n'
             print(msg)
 
+            # info_url = self.base_url[i] + '/user'
+            # response = session.get(info_url, verify=False)
         except Exception as e:
             msgall = '签到失败'
             print(e)
@@ -53,8 +52,6 @@ class SspanelQd(object):
 
     def main(self):
         msg = self.checkin()
-        with open('log.txt', 'w') as f:
-            f.write(msg)
 
 
 if __name__ == '__main__':
