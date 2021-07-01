f87545d820b126f8ae5e94f608a7e5e3e09adc91
zhangchaowei <withqianqian@163.com>
2021-07-01 15:44:04 +0800

    2

--- checkin.py
+++ checkin.py
@@ -9,42 +9,39 @@ class SspanelQd(object):
     def __init__(self):
         # 机场地址
 
-        self.base_url = os.environ['web'].split(',')[0]
+        self.base_url = os.environ['web'].split(',')
         # 登录信息
 
-        self.email = os.environ['user'].split(',')[0]
+        self.email = os.environ['user'].split(',')
 
-        self.password = os.environ['pwd'].split(',')[0]
+        self.password = os.environ['pwd'].split(',')
 
     def checkin(self):
         msgall = '签到成功'
         try:
+            for i in range(len(self.base_url)):
+                session = requests.session()
 
-            session = requests.session()
+                login_url = self.base_url[i] + '/auth/login'
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                }
 
-            login_url = self.base_url + '/auth/login'
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            }
+                post_data = 'email='+self.email[i]+'&passwd='+self.password[i]
+                post_data = post_data.encode()
+                response = session.post(login_url, post_data, headers=headers, verify=False)
 
-            post_data = 'email='+self.email+'&passwd='+self.password
-            post_data = post_data.encode()
-            response = session.post(login_url, post_data, headers=headers, verify=False)
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                    'Referer': self.base_url[i] + '/user'
+                }
 
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Referer': self.base_url + '/user'
-            }
+                response = session.post(self.base_url[i] + '/user/checkin', headers=headers, verify=False)
+                msg = (response.json()).get('msg')
 
-            response = session.post(self.base_url + '/user/checkin', headers=headers, verify=False)
-            msg = (response.json()).get('msg')
+                print(self.base_url[i]+' \n'+msg)
 
-            msgall = msgall + self.base_url + '\n\n' + msg + '\n\n'
-            print(msg)
-
-            # info_url = self.base_url[i] + '/user'
-            # response = session.get(info_url, verify=False)
         except Exception as e:
             msgall = '签到失败'
             print(e)
